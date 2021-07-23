e3d7084fcd4d3f3ea6ee1a5cecfae2319bda5363
Evgen <Kadett0303@gmail.com>
2021-07-24 01:20:26 +0300

    Finish class ArrayInt. Task 1.

--- Task1/ArrayInt.cpp
+++ Task1/ArrayInt.cpp
@@ -1,5 +1,105 @@
-//
-// Created by User on 023 23.07.21.
-//
-
 #include "ArrayInt.h"
+
+ArrayInt::ArrayInt() : m_length(0), m_data(nullptr) {}
+
+ArrayInt::ArrayInt(int length) {
+    assert(length >= 0);
+    if (length > 0)
+        m_data = new int[length];
+    else
+        m_data = nullptr;
+}
+
+ArrayInt::~ArrayInt() {
+    delete[] m_data;
+}
+
+void ArrayInt::erase() {
+    delete[] m_data;
+    m_data = nullptr;
+    m_length = 0;
+}
+
+int ArrayInt::getLength() {
+    return m_length;
+}
+
+int &ArrayInt::operator[](int index) {
+    assert(index >= 0 && index < m_length);
+    return m_data[index];
+}
+
+void ArrayInt::resize(int newLength) {
+    if (newLength == m_length)
+        return;
+
+    if (newLength <= 0) {
+        erase();
+        return;
+    }
+
+    int *data = new int[newLength];
+
+    if (m_length > 0) {
+        int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+
+        for (int index = 0; index < elementsToCopy; ++index)
+            data[index] = m_data[index];
+    }
+
+    delete[] m_data;
+    m_data = data;
+    m_length = newLength;
+}
+
+void ArrayInt::insertBefore(int value, int index) {
+
+    assert(index >= 0 && index <= m_length);
+
+    int *data = new int[m_length + 1];
+
+    for (int before = 0; before < index; ++before)
+        data[before] = m_data[before];
+
+    data[index] = value;
+
+    for (int after = index; after < m_length; ++after)
+        data[after + 1] = m_data[after];
+
+    delete[] m_data;
+    m_data = data;
+    ++m_length;
+}
+
+void ArrayInt::push_back(int value) {
+    insertBefore(value, m_length);
+}
+
+void ArrayInt::pop_back() {
+    resize(m_length - 1);
+}
+
+void ArrayInt::pop_front() {
+    if(m_length <= 0) return;
+    int *data = new int[m_length - 1];
+    for (int i = 0; i < m_length; ++i) {
+        data[i] = m_data[i + 1];
+    }
+    delete[] m_data;
+    m_data = data;
+    --m_length;
+}
+
+void ArrayInt::sort() {
+    std::sort(m_data, m_data + m_length);
+}
+
+void ArrayInt::print() {
+    std::cout << "[ ";
+    for (int i = 0; i < m_length; ++i) {
+        std::cout << m_data[i];
+        if (i != m_length - 1) std::cout << ", ";
+
+    }
+    std::cout << " ]";
+}
--- Task1/ArrayInt.h
+++ Task1/ArrayInt.h
@@ -1,6 +1,28 @@
 #pragma once
-class ArrayInt {
+#include <iostream>
+#include <cassert>
+#include <algorithm>
 
+class ArrayInt {
+    int m_length;
+    int *m_data;
+public:
+    ArrayInt();
+    ArrayInt(int length);
+    ~ArrayInt();
+
+    void erase();
+    int getLength();
+    int& operator[](int index);
+
+    void resize(int newLength);
+
+    void insertBefore(int value, int index);
+    void push_back(int value);
+    void pop_back();
+    void pop_front();
+    void sort();
+    void print();
 };
 
 
