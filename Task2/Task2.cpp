181dde355da4a5daba12fedf3092c7fc721f8c48
Evgen <Kadett0303@gmail.com>
2021-07-24 13:52:15 +0300

    Finished functions initVectorRandom() and countUniqueNumbers()

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -1,2 +1,18 @@
 #include "Task2.h"
 
+void initVectorRandom(std::vector<int> &v, uint32_t max_number, size_t size) {
+    srand(time(0));
+    v.clear();
+    v.reserve(size);
+    for (int i = 0; i < size; ++i) {
+        v.push_back(rand() % max_number);
+    }
+}
+
+int countUniqueNumbers(std::vector<int> &vector){
+    std::set<int> s;
+    for(auto &i : vector){
+        s.insert(i);
+    }
+    return s.size();
+}
--- Task2/Task2.h
+++ Task2/Task2.h
@@ -3,5 +3,5 @@
 #include <set>
 #include <ctime>
 
-void initVectorRandom(std::vector<int>&, size_t);
-int countUniqueNumbers(std::vector<int>&);
+void initVectorRandom(std::vector<int>& vector, uint32_t max_number, size_t size);
+int countUniqueNumbers(std::vector<int>& vector);
